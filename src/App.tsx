e866bbc11f380f52883f78c63c62badd5685c5ac
Roger Omar Florez Garcia <roger_flasx@hotmail.com>
2025-01-30 01:03:12 -0500

    Clase 7: Custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from './components'
 import { useFetch } from './hooks'
 
 const url = "https://api.example.com/data"
@@ -13,22 +11,6 @@ interface Data {
 
 function App() {
   const { data, loading, error } = useFetch<Data>(url)
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("Roger")
-
-  const changeName = () => {
-    setName("Omar")
-  }
-
-  const countMore = () => {
-    setCount((count) => count + 1)
-  }
-
-  const countLess = () => {
-    if(count > 0){
-      setCount(count - 1)
-    }
-  }
 
   if (loading) {
     return <div>Cargando...</div>
@@ -39,24 +21,9 @@ function App() {
   }
 
   return (
-    <>
-      <div className="card">
-        <p className='pb-8'>{name}</p>
-        <Button label='Cambiar Nombre' parentMethod={changeName} />
-
-        <div className='flex justify-around gap-5 pt-10 m-auto'>
-          <Button label='Suma' parentMethod={countMore} />
-
-          <Button label='Resta' parentMethod={countLess} />
-        </div>
-
-        <p className='pt-8'>Count is {count}</p>
-
-        <div>
-          {JSON.stringify(data)}
-        </div>
-      </div>
-    </>
+    <div>
+      {JSON.stringify(data)}
+    </div>
   )
 }
 
