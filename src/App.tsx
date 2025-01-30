7bbc7e0f0b80fa378b514c86145cefa4e049d288
Roger Omar Florez Garcia <roger_flasx@hotmail.com>
2025-01-30 01:06:50 -0500

    Clase 6: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
+import { useState } from 'react'
 import './App.css'
-import { useFetch } from './hooks'
-
-const url = "https://api.example.com/data"
-
-interface Data {
-  name: string
-  lastName: string
-  age: number
-}
+import { Button } from './components';
 
 function App() {
-  const { data, loading, error } = useFetch<Data>(url)
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Roger")
 
-  if (loading) {
-    return <div>Cargando...</div>
+  const changeName = () => {
+    setName("Omar");
   }
 
-  if (error) {
-    return <div>{error.message}</div>
+  const countMore = () => {
+    setCount((count) => count + 1)
+  }
+
+  const countLess = () => {
+    if(count > 0){
+      setCount(count - 1);
+    }
   }
 
   return (
-    <div>
-      {JSON.stringify(data)}
-    </div>
+    <>
+      <div className="card">
+        <p className='pb-8'>{name}</p>
+        <Button label='Cambiar Nombre' parentMethod={changeName} />
+
+        <div className='flex justify-around gap-5 pt-10 m-auto'>
+          <Button label='Suma' parentMethod={countMore} />
+
+          <Button label='Resta' parentMethod={countLess} />
+        </div>
+
+        <p className='pt-8'>Count is {count}</p>
+      </div>
+    </>
   )
 }
 
